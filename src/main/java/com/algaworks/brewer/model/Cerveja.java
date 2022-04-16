bd12de04957ae9e5f64647440fbdddf9bfc603f6
Bruno Valentino <b.vcsilva@outlook.com>
2022-04-16 17:22:36 -0300

    feat: adiciona callbacks JPA para validar SKU em uppercase

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -12,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
@@ -178,6 +180,12 @@ public class Cerveja {
 			return false;
 		Cerveja other = (Cerveja) obj;
 		return Objects.equals(codigo, other.codigo);
+	}
+	
+	@PrePersist
+	@PreUpdate
+	private void prePersistUpdateEntity() {
+		this.sku = this.sku.toUpperCase();
 	}
 			
 }
