772db3553c46fe1e46a679e7f3dedc39bf931536
Bruno Valentino <b.vcsilva@outlook.com>
2022-04-16 18:31:29 -0300

    refactor: modulariza código JS para cadastro rápido de estilo

--- src/main/resources/static/javascripts/estilo.cadastro-rapido.js
+++ src/main/resources/static/javascripts/estilo.cadastro-rapido.js
@@ -1,48 +1,70 @@
-$(function() {
-    const inputNomeEstilo = $('#nomeEstilo');    
-    const mensagemCadastroEstilo = $('.js-mensagem-cadastro-rapido-estilo');
+var Brewer = Brewer || {};
 
-    const modal = $('#modalCadastroRapidoEstilo');
-    modal.on('shown.bs.modal', function() {
-        inputNomeEstilo.focus();
-    });
-	modal.on('hide.bs.modal', function() {
-        inputNomeEstilo.val('');
-        mensagemCadastroEstilo.addClass('hidden');
-        formulario.find('.form-group').removeClass('has-error');
-    });
+Brewer.EstiloCadastroRapido = (function() {
+	function EstiloCadastroRapido() {
+		this.inputNomeEstilo = $('#nomeEstilo');    
+	    this.mensagemCadastroEstilo = $('.js-mensagem-cadastro-rapido-estilo');	
+	    this.modal = $('#modalCadastroRapidoEstilo');	    		
+	    this.formulario = this.modal.find('form');	    	
+	    this.urlCadastroEstilo = this.formulario.attr('action');	        
+	    this.botaoSalvar = this.modal.find('.js-modal-cadastro-rapido-estilo-btn');
+	}
 	
-    const formulario = modal.find('form');
-    formulario.on('submit', function(event) {
-        event.preventDefault();
-    });
+	EstiloCadastroRapido.prototype.iniciar = function() {
+		this.modal.on('shown.bs.modal', onModalShow.bind(this));    	    	   
+	    this.modal.on('hide.bs.modal', onModalClose.bind(this));	    
+	    
+	    this.formulario.on('submit', function(event) {
+	        event.preventDefault();
+	    });
+	    
+	    this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this)); 
+	}
+	
+	function onModalShow() {
+		this.inputNomeEstilo.focus();
+	}
+	
+	function onModalClose() {
+		this.inputNomeEstilo.val('');
+	    this.mensagemCadastroEstilo.addClass('hidden');
+        this.formulario.find('.form-group').removeClass('has-error');
+	}
+	
+	function onBotaoSalvarClick() {
+		const nomeEstilo = this.inputNomeEstilo.val().trim();
+	       
+	   	$.ajax({
+	       url: this.urlCadastroEstilo,
+	       method: 'POST',
+	       contentType: 'application/json',
+	       data: JSON.stringify({ 'nome': nomeEstilo }),
+	       error: onSalvarEstiloError.bind(this),
+		   success: onSalvarEstiloSuccess.bind(this)        
+	   });
+	}
+	
+	function onSalvarEstiloError(object) {
+   		const mensagemDeErro = object.responseText; 
+   		
+   		this.mensagemCadastroEstilo.removeClass('hidden');
+   		this.mensagemCadastroEstilo.html('<span><i class="fa  fa-exclamation-circle"></i> ' + mensagemDeErro + '</span>');
+   		
+   		this.formulario.find('.form-group').addClass('has-error');
+    }
+   
+    function onSalvarEstiloSuccess(estilo) {
+		const comboEstilo = $('#estilo');
+		comboEstilo.append('<option value=' + estilo.codigo + '>' + estilo.nome + '</option>');
+		comboEstilo.val(estilo.codigo);
+		
+		this.modal.modal('hide');
+    }	
+	
+	return EstiloCadastroRapido;
+}());
 
-    const urlCadastroEstilo = formulario.attr('action');
-        
-    const botaoSalvar = modal.find('.js-modal-cadastro-rapido-estilo-btn');
-    botaoSalvar.on('click', function() {
-       const nomeEstilo = inputNomeEstilo.val().trim();
-       
-       $.ajax({
-           url: urlCadastroEstilo,
-           method: 'POST',
-           contentType: 'application/json',
-           data: JSON.stringify({ 'nome': nomeEstilo }),
-           error: function(object) {
-		   		const mensagemDeErro = object.responseText; 
-		   		
-		   		mensagemCadastroEstilo.removeClass('hidden');
-		   		mensagemCadastroEstilo.html('<span><i class="fa  fa-exclamation-circle"></i> ' + mensagemDeErro + '</span>');
-		   		
-		   		formulario.find('.form-group').addClass('has-error');
-		   },
-		   success: function(estilo) {
-				const comboEstilo = $('#estilo');
-				comboEstilo.append('<option value=' + estilo.codigo + '>' + estilo.nome + '</option>');
-				comboEstilo.val(estilo.codigo);
-				
-				modal.modal('hide');
-		   }       
-       });
-    });       
+$(function() {
+	var estiloCadastroRapido = new Brewer.EstiloCadastroRapido();
+	estiloCadastroRapido.iniciar();	    
 });
